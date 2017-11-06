79cd88b4f96c959b45f3f6950edbcaf048bbe19e
chahni <chahni@users.noreply.github.com>
2017-11-06 11:39:37 -0700

    Initial commit

---
+++ GettingDressed.UnitTest/HotTemperatureUnitTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GettingDressed.Rules;
+using GettingDressed.Library.Errors;
+
+namespace GettingDressed.UnitTest
+{
+    [TestClass]
+    public class HotTemperatureUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            String actualValue = "Removing PJs,shorts,shirt,sunglasses,sandals,Leaving House";
+            String temperatureType = "HOT";
+            String commandList = "8,6,4,2,1,7";
+            IRulesEngine iRules = new RulesEngine();
+            String expectedValue = iRules.processRules(temperatureType, commandList);
+            Assert.AreEqual(actual: actualValue, expected: expectedValue);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            String actualValue = "Removing PJs,shorts,fail";
+            String temperatureType = "HOT";
+            String commandList = "8,6,6";
+            IRulesEngine iRules = new RulesEngine();
+            String expectedValue = iRules.processRules(temperatureType, commandList);
+            Assert.AreEqual(actual: actualValue, expected: expectedValue);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            String actualValue = "Removing PJs,shorts,fail";
+            String temperatureType = "HOT";
+            String commandList = "8,6,3";
+            IRulesEngine iRules = new RulesEngine();
+            String expectedValue = iRules.processRules(temperatureType, commandList);
+            Assert.AreEqual(actual: actualValue, expected: expectedValue);
+        }
+
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            String actualValue = "Removing PJs,shorts,fail";
+            String temperatureType = "HOT";
+            String commandList = "8,6,20";
+            IRulesEngine iRules = new RulesEngine();
+            String expectedValue = iRules.processRules(temperatureType, commandList);
+            Assert.AreEqual(actual: actualValue, expected: expectedValue);
+        }
+
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            String actualValue = "Removing PJs,shorts,shirt,fail";
+            String temperatureType = "HOT";
+            String commandList = "8,6,4,3,5,2,1,7";
+            IRulesEngine iRules = new RulesEngine();
+            String expectedValue = iRules.processRules(temperatureType, commandList);
+            Assert.AreEqual(actual: actualValue, expected: expectedValue);
+        }
+
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            String actualValue = ErrorConstants.FAILED;
+            String temperatureType = "HOT";
+            String commandList = "6";
+            IRulesEngine iRules = new RulesEngine();
+            String expectedValue = iRules.processRules(temperatureType, commandList);
+            Assert.AreEqual(actual: actualValue, expected: expectedValue);
+        }
+    }
+}
---
+++ GettingDressed/Program.cs
@@ -0,0 +1,90 @@
+﻿using GettingDressed.Rules;
+using System;
+using Ninject;
+using Ninject.Modules;
+namespace GettingDressed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                executeHotTemperatureProcess();
+                executeColdTemperatureProcess();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.Read();
+            }
+        }
+
+        private static void executeColdTemperatureProcess()
+        {
+            Console.WriteLine("Cold temperature processing start");
+            Console.WriteLine();
+            String temperatureType = string.Empty;
+            String commandList = string.Empty;
+            IRulesEngine iRules = new RulesEngine();
+            Console.WriteLine("****SUCCESS****");
+            temperatureType = "COLD";
+            commandList = "8,6,3,4,2,5,1,7";
+            Console.WriteLine(iRules.processRules(temperatureType, commandList));
+
+            Console.WriteLine("****FAILURE****");
+            temperatureType = "COLD";
+            commandList = "8,6,3,4,2,5,7";
+            Console.WriteLine(iRules.processRules(temperatureType, commandList));
+
+            temperatureType = "COLD";
+            commandList = "6";
+            Console.WriteLine(iRules.processRules(temperatureType, commandList));
+            Console.WriteLine("Cold temperature processing end");
+            Console.WriteLine();
+        }
+
+        private static void executeHotTemperatureProcess()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Hot temperature processing start");
+            String temperatureType = string.Empty;
+            String commandList = string.Empty;
+
+            IRulesEngine iRules = new RulesEngine();
+            Console.WriteLine(iRules.processRules(temperatureType, commandList));
+
+            Console.WriteLine("****SUCCESS****");
+            temperatureType = "HOT"; 
+            commandList = "8,6,4,2,1,7"; 
+
+            Console.WriteLine(iRules.processRules(temperatureType, commandList));
+            Console.WriteLine();
+
+            Console.WriteLine("****FAILURE****");
+            temperatureType = "HOT"; 
+            commandList = "8,6,6"; 
+
+            Console.WriteLine(iRules.processRules(temperatureType, commandList));
+
+            temperatureType = "HOT"; 
+            commandList = "8,6,3"; 
+            Console.WriteLine(iRules.processRules(temperatureType, commandList));
+
+
+            temperatureType = "HOT"; 
+            commandList = "8,6,6,20"; 
+            Console.WriteLine(iRules.processRules(temperatureType, commandList));
+
+
+            temperatureType = "HOT"; 
+            commandList = "8,6,4,3,5,2,1,7"; 
+            Console.WriteLine(iRules.processRules(temperatureType, commandList));
+            Console.WriteLine("Hot temperature processing end");
+            Console.WriteLine();
+        }
+    }
+}
